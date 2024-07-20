fc01d41e9b8a9b1505ec69e19ad5b043a5fa3e0e
Unchanted <omkkdwivedi@gmail.com>
2024-07-20 13:04:17 +0530

    logic improved

--- Main.cpp
+++ Main.cpp
@@ -19,13 +19,14 @@ int main()
     System sys;
 
     Map::canvas = sys.get_file_content("Map.txt");
+    Head::set_coord();
+
     Fruit fruit;
     int lives = 1;
     int score = 0;
     int difficulty = 80;
     char key_pressed = MOVE_UP;
-    int x = set_x();
-    int y = set_y();
+
     sys.show_consol_cursor(false);
     system("cls");
     Map::print();
@@ -43,69 +44,11 @@ int main()
 
         if (_kbhit())
             key_pressed = toupper(_getch());
-
-        switch (key_pressed)
-        {
-        case MOVE_UP:
-            //Head::move_up();
-            gotoxy(x, y);
-            cout<<' ';
-            y -= 1;
-            gotoxy(x, y);
-            if (getCursorChar() == '#')
-                lives--;
-            else
-            {
-                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 114);
-                cout<<'0';
-            }
-
-            break;
-        case MOVE_LEFT:
-            //Head::move_left();
-            gotoxy(x, y);
-            cout<<' ';
-            x -= 1;
-            gotoxy(x, y);
-            if (getCursorChar() == '#')
-                lives--;
-            else
-            {
-                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 114);
-                cout<<'0';
-            }
-            break;
-        case MOVE_DOWN:
-            gotoxy(x, y);
-            cout<<' ';
-            y += 1;
-            gotoxy(x, y);
-            if (getCursorChar() == '#')
-                lives--;
-            else
-            {
-                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 114);
-                cout<<'0';
-            }
-            //Head::move_down();
-            break;
-        case MOVE_RIGHT:
-            gotoxy(x, y);
-            cout<<' ';
-            x += 1;
-            gotoxy(x, y);
-            if (getCursorChar() == '#')
-                lives--;
-            else
-            {
-                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 114);
-                cout<<'0';
-            }
-            //Head::move_right();
-            break;
-        default:
-            break;
-        }
+        Head::move(key_pressed);
+        if (Head::get_colision())
+            lives--;
+        else
+            Head::print();
 
         gotoxy(0, Map::get_height() + 10);
         cout << "Hp: " << Head::get_position() << "\n";
@@ -114,7 +57,7 @@ int main()
         cout << "w: " << Map::get_width() << "\n";
         cout << "h_w: " << Head::get_position() % Map::get_width() << "\n";
         gotoxy(10, 0);
-        char ch = getCursorChar();
+        char ch = get_cursor_char();
         //gotoxy(10, 60);
         //cout<<ch;
 
